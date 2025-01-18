e11ccf8d933669153e2ed80280bf457a1b9f27d3
Dmytro <dimka670020040@gmail.com>
2025-01-18 06:46:11 +0200

    Initial commit
    Generated by create-expo-stack 2.12.1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,13 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
+<<<<<<< HEAD
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="onboarding" />
     </Stack>
   );
+=======
+  return <Stack />;
+>>>>>>> bd391e4 (Initial commit)
 }
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,13 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
+<<<<<<< HEAD
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+=======
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+>>>>>>> bd391e4 (Initial commit)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
